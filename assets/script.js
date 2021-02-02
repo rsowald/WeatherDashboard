19b700dc4f19b68473823fe6f3e81ae6751b93e4
rsowald <72467920+rsowald@users.noreply.github.com>
2021-02-02 00:03:02 -0800

    rendering 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,10 @@ function getCurrentWeather() {
         .fail(onForecastError);
 }
 
+// function getWeatherIcon{
+//     //todo: extract this from onCurrentConditionsSuccess
+// }
+
 function onCurrentConditionsError() {
     console.log("error");
     //todo:400 error from API - your error
@@ -55,8 +59,22 @@ function onForecastError() {
     //todo:400 error from API - your error
     //todo:500 error from API - API error
 }
-function onForecastSuccess(data) {
-
+function onForecastSuccess(hourForecasts) {
+    console.log(hourForecasts);
+    //these hourForecasts come in 3 hour blocks, so every 8th index is the same time on the next day
+    var dayForecasts = [hourForecasts.list[2], hourForecasts.list[10], hourForecasts.list[18], hourForecasts.list[26], hourForecasts.list[34]]
+    $("#5-day-forecast").empty();
+    for (var i = 0; i < dayForecasts.length; i++) {
+        var forecast = dayForecasts[i]
+        var date = new Date(forecast.dt_txt).toLocaleDateString();
+        var weatherIcon = "http://openweathermap.org/img/wn/" + forecast.weather[0].icon + "@2x.png";
+        var temp = forecast.main.temp;
+        var humidity = forecast.main.humidity;
+        var card = $("<div class='card forecastCard'></div>");
+        card.append("<h2>" + date + "</h2>");
+        card.append("<p class='card-text'>" + "<img src=" + weatherIcon + "></img><br>Temp: " + temp + "°F<br>Humidity: " + humidity + "%</p>");
+        $("#5-day-forecast").append(card);
+    }
 }
 
 
