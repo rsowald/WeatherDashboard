6da81726562edc39f2f2fc69755ecc2c2f8bbf64
rsowald <72467920+rsowald@users.noreply.github.com>
2021-02-03 15:58:08 -0800

    call UVindex api and display results

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     var apiKey = "d12a2200f8bbf27b2b5fa7c741e3a391";
     var currentConditionsApiUrl = "https://api.openweathermap.org/data/2.5/weather";
     var forecastApiUrl = "https://api.openweathermap.org/data/2.5/forecast";
+
     var searchHistory;
 
     function renderSearchHistory() {
@@ -10,18 +11,16 @@ $(document).ready(function () {
         $("#search-history").empty();
         // Render a new button for each city
         searchHistory = JSON.parse(localStorage.getItem("Searched Cities"));
-
+        //make button only if it is not already in history list
         if (!searchHistory) {
             searchHistory = [];
         }
 
         for (var i = 0; i < searchHistory.length; i++) {
             var city = searchHistory[i];
-
             var button = $("<button></button>");
             button.addClass("btn btn-outline-light");
             button.text(city);
-
             $("#search-history").append(button);
         }
     }
@@ -43,22 +42,48 @@ $(document).ready(function () {
     }
 
     // function getWeatherIcon{
-    //     //todo: extract this from onCurrentConditionsSuccess and onForecastSuccess
+    //     //TODO: extract this from onCurrentConditionsSuccess and onForecastSuccess
     // }
 
     function onCurrentConditionsError() {
         console.log("error");
-        //todo:400 error from API - your error
-        //todo:500 error from API - API error
+        // TODO: 400 error from API - your error
+        //TODO:500 error from API - API error
     }
     function onCurrentConditionsSuccess(data, cityName) {
         function displayCurrentConditions() {
             console.log(data);
             var date = new Date();
-            var weatherIcon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
-            var lat = data.coord.lat;
-            var lon = data.coord.lon;
-            //todo: call UVIndex API and display result
+            var weatherIcon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+            var weatherRequest = {
+                lat: data.coord.lat,
+                lon: data.coord.lon,
+                appid: apiKey
+            }
+            var uvIndex = "https://api.openweathermap.org/data/2.5/onecall"
+
+            $.get(uvIndex, weatherRequest)
+                .done(uvData => {
+                    $("#current-weather").append($("<p>UV Index: <span id='uvIndex'>" + uvData.current.uvi + "</span></p>"));
+                    var uvDisplay = $("#uvIndex");
+                    if (uvData.current.uvi <= 2) {
+                        uvDisplay.attr("style", "background-color:green;")
+                    }
+                    else if (uvData.current.uvi > 2 && uvData.current.uvi <= 5) {
+                        uvDisplay.attr("style", "background-color:yellow;")
+                    }
+                    else if (uvData.current.uvi > 5 && uvData.current.uvi <= 7) {
+                        uvDisplay.attr("style", "background-color:orange;")
+                    }
+                    else if (uvData.current.uvi > 7 && uvData.current.uvi <= 10) {
+                        uvDisplay.attr("style", "background-color:orange;")
+                    }
+                    else {
+                        uvDisplay.attr("style", "background-color:light-purple;")
+                    }
+                })
+                .fail(error => console.log(error));
+            //TODO: alert errors
             $("#current-weather").empty();
             $("#current-weather").append($("<h2>" + data.name + " (" + date.toLocaleDateString() + ")" + "<img src=" + weatherIcon + "></img></h2>"));
             $("#current-weather").append($("<p>Temperature: " + data.main.temp + " °F</p>"));
@@ -69,23 +94,24 @@ $(document).ready(function () {
             var newSearch = $("<button>" + cityName + "</button>");
             newSearch.addClass("btn btn-outline-light");
             $("#search-history").append(newSearch);
-
-            //todo: only add to history if it's not already there
-
         }
         function storeHistory() {
             searchHistory.push(cityName);
             localStorage.setItem("Searched Cities", JSON.stringify(searchHistory))
         }
+
         displayCurrentConditions();
-        addHistoryButton();
-        storeHistory();
+
+        if (!searchHistory.includes(cityName)) {
+            addHistoryButton();
+            storeHistory();
+        }
     }
 
     function onForecastError() {
         console.log("error");
-        //todo:400 error from API - your error
-        //todo:500 error from API - API error
+        //TODO:400 error from API - your error
+        //TODO:500 error from API - API error
     }
     function onForecastSuccess(hourForecasts) {
         console.log(hourForecasts);
@@ -96,7 +122,7 @@ $(document).ready(function () {
         for (var i = 0; i < dayForecasts.length; i++) {
             var forecast = dayForecasts[i]
             var date = new Date(forecast.dt_txt).toLocaleDateString();
-            var weatherIcon = "http://openweathermap.org/img/wn/" + forecast.weather[0].icon + "@2x.png";
+            var weatherIcon = "http://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
             var temp = forecast.main.temp;
             var humidity = forecast.main.humidity;
             var card = $("<div class='card forecastCard'></div>");
@@ -107,14 +133,15 @@ $(document).ready(function () {
     }
 
     renderSearchHistory();
-    $("#button-addon2").on("click", function () {
+    $("#button-addon2").on("click", function (event) {
+        event.preventDefault();
         var cityName = $("#cityName").val();
         $("#cityName").val("");
         getCurrentWeather(cityName);
     });
-    //todo: make enter also submit search
     $("#search-history").on("click", "button", function () {
         getCurrentWeather($(this).text());
+
     });
 
 });
